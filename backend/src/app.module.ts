11d6b48fed5c5c47c6f2454411cd937a2aa59f4f
Tanya Koshti <tanyakoshti115@gmail.com>
2026-04-10 23:53:10 +0530

    Update app.module.ts with config and throttling

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { envValidationSchema } from './env.validation';
 import { AnalyticsModule } from './modules/analytics/analytics.module';
 import { FeedbackModule } from './modules/feedback/feedback.module';
 import { DeliveriesModule } from './modules/deliveries/deliveries.module';
@@ -13,6 +17,8 @@ import { AlertsModule } from './modules/alerts/alerts.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true, validationSchema: envValidationSchema }),
+    ThrottlerModule.forRoot({ ttl: 60, limit: 60 }),
     AuthModule,
     UsersModule,
     ProductsModule,
@@ -25,5 +31,6 @@ import { AlertsModule } from './modules/alerts/alerts.module';
     FraudModule,
     AlertsModule,
   ],
+  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
