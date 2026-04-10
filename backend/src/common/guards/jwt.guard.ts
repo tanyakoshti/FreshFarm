f9df0a7e044ec762c422f6b7d62426484a4ffc44
Tanya Koshti <tanyakoshti115@gmail.com>
2026-04-11 00:00:33 +0530

    Harden JWT guard

--- backend/src/common/guards/jwt.guard.ts
+++ backend/src/common/guards/jwt.guard.ts
@@ -1,8 +1,9 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtGuard implements CanActivate {
-  canActivate(_context: ExecutionContext) {
-    return true;
+export class JwtGuard extends AuthGuard('jwt') {
+  canActivate(context: ExecutionContext) {
+    return super.canActivate(context);
   }
 }
